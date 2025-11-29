bb7e92e4f79ee2cc21964d6711c35c85eee26ae2
Sriveer Neerukonda <sriveer.neerukonda@outlook.com>
2025-11-29 15:00:13 +0530

    feat: dont allow user to stay in Account page if they are not logged in

--- src/component/pages/User/Account.tsx
+++ src/component/pages/User/Account.tsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AuthService from "../../../service/AuthService";
+import { useNavigate } from "react-router";
+import { appGlobalStateContext } from "../../App/AppGlobalStateProvider";
+import IAppGlobalStateContextAPI from "../../../interface/IAppGlobalStateContextAPI";
 
 export default function Account() {
+
+    // Global API
+    const { getLoggedInDetails } = React.useContext(appGlobalStateContext) as IAppGlobalStateContextAPI;
+    const authService = AuthService.getInstance();
+    const navigateTo = useNavigate();
+
+    // Effects
+    useEffect(() => {
+        // Redirect to login if not logged in
+        (async () => {
+            const userDetails = await authService.getCurrentUser();
+            if(!userDetails) {
+                navigateTo("/account/login", {replace: true});
+            }
+        })();
+    },[getLoggedInDetails()]);
+
     return (
         <>
             <h1>Account Page</h1>
