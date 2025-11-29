8696fe323da5878754a2c684aa34aea022fa0fa3
Sriveer Neerukonda <sriveer.neerukonda@outlook.com>
2025-11-29 14:49:57 +0530

    feat: add animation to login

--- src/component/pages/User/PageShell.tsx
+++ src/component/pages/User/PageShell.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { Container, Card, CardContent, Typography, Box, Button, TextField } from "@mui/material";
-import { Link } from "react-router";
+import React from "react";
+import { Container, Card, CardContent, Typography } from "@mui/material";
+import { motion } from 'framer-motion';
 
 export interface PageShellProps {
     pageLabel: string;
@@ -8,13 +8,19 @@ export interface PageShellProps {
 
 export default function PageShell({ pageLabel, children }: React.PropsWithChildren<PageShellProps>) {
     return (
-        <Container maxWidth="sm" sx={{ py: 4, px: {xs: 0, sm: 3} }}>
-            <Card>
-                <CardContent sx={{ p: { xs: 3, sm: 4 } }}>
-                    <Typography variant="h1" component="h1" textAlign="center" sx={{ mb: 3 }}>{pageLabel}</Typography>
-                    {children}
-                </CardContent>
-            </Card>
-        </Container>
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          key={location.pathname}
+        >
+            <Container maxWidth="sm" sx={{ py: 4, px: {xs: 0, sm: 3} }}>
+                <Card>
+                    <CardContent sx={{ p: { xs: 3, sm: 4 } }}>
+                        <Typography variant="h1" component="h1" textAlign="center" sx={{ mb: 3 }}>{pageLabel}</Typography>
+                        {children}
+                    </CardContent>
+                </Card>
+            </Container>
+        </motion.div>
     );
 }
