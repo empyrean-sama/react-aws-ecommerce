1199451105837366d6c329e440e5e462d68a2092
Sriveer Neerukonda <sriveer.neerukonda@outlook.com>
2025-12-02 13:09:24 +0530

    feat: add non responsive order cards on account

--- src/component/pages/User/Account.tsx
+++ src/component/pages/User/Account.tsx
@@ -3,6 +3,8 @@ import AuthService from "../../../service/AuthService";
 import { useNavigate } from "react-router";
 import { appGlobalStateContext } from "../../App/AppGlobalStateProvider";
 import IAppGlobalStateContextAPI from "../../../interface/IAppGlobalStateContextAPI";
+import { Container, Typography, Box } from "@mui/material";
+import OrderCard from "./OrderCard";
 
 export default function Account() {
 
@@ -23,8 +25,65 @@ export default function Account() {
     },[getLoggedInDetails()]);
 
     return (
-        <>
-            <h1>Account Page</h1>
-        </>
+        <Container maxWidth="xl">
+            <Typography variant="h1" component="h1" gutterBottom>
+                Hi {getLoggedInDetails()?.givenName} {getLoggedInDetails()?.familyName},
+            </Typography>
+            <Box sx={{display: 'flex', flexDirection: 'column', gap: 1, mt: 2}}>
+                <OrderCard
+                    status="order placed"
+                    itemData={[
+                        { name: "Product 1", cost: "50", quantity: "1", totalCost: "50" },
+                        { name: "Product 2", cost: "25", quantity: "2", totalCost: "50" },
+                    ]}
+                    orderSubtotal="100$"
+                    extraFees={{ Shipping: "10$", Tax: "5$" }}
+                    orderTotal="115$"
+                    orderId="#123456"
+                    orderDate={new Date(Date.now())}
+                    shippingAddress={{
+                        userLabel: "Home",
+                        specificAddress: "Flat number 201, kiragi apartments",
+                        street: "Near Hotel Transylvania, MG Road",
+                        area: "George Highlands",
+                        city: "Raidurgam",
+                        state: "Telangana",
+                        country: "India",
+                        postcode: "530003"
+                    }}
+                    paymentDetails="Visa •••• 1234"
+                    paymentMode="Pre Paid"
+                    phoneNumber="+1 234 567 8901"
+                    email="simba@example.com"
+                />
+
+                <OrderCard
+                    status="delivered"
+                    itemData={[
+                        { name: "Product 1", cost: "50", quantity: "1", totalCost: "50" },
+                        { name: "Product 2", cost: "25", quantity: "2", totalCost: "50" },
+                    ]}
+                    orderSubtotal="100$"
+                    extraFees={{ Shipping: "10$", Tax: "5$" }}
+                    orderTotal="115$"
+                    orderId="#123456"
+                    orderDate={new Date(Date.now())}
+                    shippingAddress={{
+                        userLabel: "Home",
+                        specificAddress: "Flat number 340, kiragi apartments",
+                        street: "Near Hotel Transylvania, MG Road",
+                        area: "George Highlands",
+                        city: "Raidurgam",
+                        state: "Telangana",
+                        country: "India",
+                        postcode: "530003"
+                    }}
+                    paymentDetails="COD"
+                    paymentMode="Cash on Delivery"
+                    phoneNumber="+1 234 567 8901"
+                    email="simba@example.com"
+                />
+            </Box>
+        </Container>
     );
 }
---
+++ src/component/pages/User/OrderCard.tsx
@@ -0,0 +1,226 @@
+import React from "react";
+
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    Typography,
+    Box,
+    Chip,
+    Divider,
+    Collapse,
+    Avatar,
+    Button,
+    CardActionArea,
+    LinearProgress,
+    Table,
+    TableBody,
+    TableCell,
+    TableRow,
+    TableContainer,
+    Paper,
+    TableHead,
+} from "@mui/material";
+
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+
+import EmailIcon from "@mui/icons-material/Email"; // order placed
+import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled"; // processing
+import LocalShippingIcon from "@mui/icons-material/LocalShipping"; // shipped
+import CheckCircleIcon from "@mui/icons-material/CheckCircle"; // delivered
+import HighlightOffIcon from "@mui/icons-material/HighlightOff"; // cancelled
+
+import { formatDate } from "date-fns";
+import IAddress from "../../../interface/IAddress";
+
+type OrderStatus = "order placed" | "processing" | "shipped" | "delivered" | "cancelled";
+
+interface ItemData {
+    name: string;
+    cost: string;
+    quantity: string;
+    totalCost: string;
+}
+
+interface IOrderCardProps {
+    orderId: string;
+    orderDate: Date;
+    shippingAddress: IAddress;
+    paymentMode: 'Pre Paid' | 'Cash on Delivery';
+    paymentDetails: string;
+    itemData: ItemData[];
+    orderSubtotal: string;
+    extraFees?: Record<string, string>;
+    orderTotal: string;
+    status: OrderStatus;
+    phoneNumber: string;
+    email: string;
+}
+
+function getColorAndIconByStatus(status: OrderStatus): { color: "primary" | "warning" | "info" | "success" | "error"; icon: React.ReactElement } {
+    if (status === "order placed") {
+        return { color: "primary", icon: <EmailIcon /> };
+    }
+    if (status === "processing") {
+        return { color: "warning", icon: <AccessTimeFilledIcon /> };
+    }
+    if (status === "shipped") {
+        return { color: "info", icon: <LocalShippingIcon /> };
+    }
+    if (status === "delivered") {
+        return { color: "success", icon: <CheckCircleIcon /> };
+    }
+    if (status === "cancelled") {
+        return { color: "error", icon: <HighlightOffIcon /> };
+    }
+    throw new Error("Invalid status");
+}
+
+function getProgressByStatus(status: OrderStatus): number {
+    if (status === "order placed") {
+        return 0;
+    }
+    if (status === "processing") {
+        return 33;
+    }
+    if (status === "shipped") {
+        return 66;
+    }
+    return 100;
+}
+
+export default function OrderCard(props: IOrderCardProps) {
+    const [expanded, setExpanded] = React.useState<boolean>(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    return (
+        <Card sx={{ position: "relative" }}>
+            <LinearProgress
+                variant="determinate"
+                value={getProgressByStatus(props.status)}
+                sx={{ position: "absolute", top: 0, left: 0, right: 0 }}
+                color={getColorAndIconByStatus(props.status).color}
+            />
+
+            <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", gap: 1, padding: 1 }}>
+                <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+                    <Chip label={props.status} color={getColorAndIconByStatus(props.status).color} size="small" icon={getColorAndIconByStatus(props.status).icon} />
+                    <Chip label={props.paymentDetails} color={props.paymentMode === "Pre Paid" ? "info" : "warning"} size="small" />
+                </Box>
+                <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+                    <Button variant="outlined" size="small">Reorder</Button>
+                    <Button variant="outlined" size="small">Request Support</Button>
+                </Box>
+                
+            </Box>
+
+            <CardActionArea onClick={handleExpandClick}>
+                <CardHeader
+                    avatar={
+                        <Avatar aria-label="order" color={getColorAndIconByStatus(props.status).color} sx={{ backgroundColor: `${getColorAndIconByStatus(props.status).color}.main` }}>
+                            {getColorAndIconByStatus(props.status).icon}
+                        </Avatar>
+                    }
+                    title={<CardTitle />}
+                    subheader={`Placed on ${formatDate(props.orderDate, "MMMM dd, yyyy")}`}
+                    action={expanded ? <ExpandLessIcon sx={{ marginY: "auto" }} /> : <ExpandMoreIcon sx={{ marginY: "auto" }} />}
+                    sx={{ cursor: "pointer", display: "flex", alignItems: "center" }}
+                />
+            </CardActionArea>
+
+            <CardContent>
+                <Collapse in={expanded} timeout="auto">
+                    <Divider sx={{ my: 2 }} />
+                    <Typography variant="h4" component="h2" sx={{ mb: 2 }}>
+                        Order Summary
+                    </Typography>
+                    <TableContainer component={Paper}>
+                        <Table size="small" aria-label="a dense table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="left">Item</TableCell>
+                                    <TableCell align="center">Price</TableCell>
+                                    <TableCell align="center">Quantity</TableCell>
+                                    <TableCell align="right">Sub Total</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {props.itemData.map((row) => (
+                                    <TableRow key={row.name} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                                        <TableCell component="th" scope="row">
+                                            {row.name}
+                                        </TableCell>
+                                        <TableCell align="center">{row.cost}</TableCell>
+                                        <TableCell align="center">{row.quantity}</TableCell>
+                                        <TableCell align="right">{row.totalCost}</TableCell>
+                                    </TableRow>
+                                ))}
+                                <TableRow>
+                                    <TableCell colSpan={4} align="right">
+                                        <Typography variant="h5">{props.orderTotal}</Typography>
+                                    </TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+
+                    <Paper variant="elevation" sx={{ p: 2, my: 2, display: "flex", flexDirection: "column", alignItems: "flex-end", gap: 0.2 }}>
+                        <Typography variant="body1" component="div">
+                            <Box component="div" sx={{ display: "flex", justifyContent: "space-between", gap: 2 }}>
+                                <span>Subtotal:</span>
+                                <span>{props.orderSubtotal}</span>
+                            </Box>
+                            {props.extraFees &&
+                                Object.entries(props.extraFees).map(([feeName, feeAmount]) => (
+                                    <Box key={feeName} component="div" sx={{ display: "flex", justifyContent: "space-between", gap: 2 }}>
+                                        <span>{feeName}:</span>
+                                        <span>{feeAmount}</span>
+                                    </Box>
+                                ))}
+                            <Divider sx={{ my: 1 }} />
+                            <Box component="div" sx={{ display: "flex", justifyContent: "space-between", gap: 2, fontWeight: "bold" }}>
+                                <span>Total:</span>
+                                <span>{props.orderTotal}</span>
+                            </Box>
+                        </Typography>
+                    </Paper>
+
+                    <Typography variant="h4" component="h2" sx={{ my: 2 }}>
+                        Shipping Details
+                    </Typography>
+                    <Paper variant="outlined" sx={{ p: 2, mb: 2 }}>
+                        <Typography variant="h6" component="h3" gutterBottom>
+                            <strong>{props.shippingAddress.userLabel}</strong>
+                        </Typography>
+                        <Typography variant="body1" component="p">
+                            {props.shippingAddress.specificAddress}, <br />
+                            {props.shippingAddress.area}, {props.shippingAddress.street}, <br />
+                            {props.shippingAddress.city} {props.shippingAddress.postcode}, <br />
+                            {props.shippingAddress.state}, {props.shippingAddress.country}.
+                        </Typography>
+                    </Paper>
+                    <Paper variant="outlined" sx={{ p: 2 }}>
+                        <Typography variant="body1" component="p">
+                            Contact: {props.phoneNumber}<br />
+                            Email: {props.email}
+                        </Typography>
+                    </Paper>
+                </Collapse>
+            </CardContent>
+        </Card>
+    );
+}
+
+function CardTitle() {
+    return (
+        <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <Typography variant="h6" component="div">
+                Order #123456789
+            </Typography>
+        </Box>
+    );
+}
