a5e3ae2178c78b13d7146c7c49f51d43d6e67d16
Sriveer Neerukonda <sriveer.neerukonda@outlook.com>
2025-12-23 15:36:02 +0530

    feat: show off image viewer

--- src/component/pages/Home/Home.tsx
+++ src/component/pages/Home/Home.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import SearchBar from "../../ui/SearchBar";
 import ImageUploader from "./ImageUploader";
+import ImageViewer from "../Admin/ImageViewer";
 
 export default function Home() {
     return (
         <div>
             <SearchBar />
             <div style={{ marginTop: 16 }}>
-                <ImageUploader />
+                <ImageViewer 
+                    imageUrls={[
+                        "https://images.pexels.com/photos/35280980/pexels-photo-35280980.jpeg?_gl=1*1m0s82x*_ga*ODQ1MzAyNDA5LjE3NjY0ODQxNDM.*_ga_8JE65Q40S6*czE3NjY0ODQxNDMkbzEkZzEkdDE3NjY0ODQxNDUkajU4JGwwJGgw",
+                        "https://images.pexels.com/photos/33944537/pexels-photo-33944537.jpeg?_gl=1*1vq2mbf*_ga*ODQ1MzAyNDA5LjE3NjY0ODQxNDM.*_ga_8JE65Q40S6*czE3NjY0ODQxNDMkbzEkZzEkdDE3NjY0ODQyMDYkajU5JGwwJGgw",
+                        "https://images.pexels.com/photos/32784501/pexels-photo-32784501.jpeg?_gl=1*1s7h9zh*_ga*ODQ1MzAyNDA5LjE3NjY0ODQxNDM.*_ga_8JE65Q40S6*czE3NjY0ODQxNDMkbzEkZzEkdDE3NjY0ODQyMzEkajM0JGwwJGgw"
+                    ]}
+                    imageWidth="400px"
+                    aspectRatio="3 / 2"
+                />
             </div>
         </div>
     )
